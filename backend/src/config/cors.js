f64c697ef0f5ad182e271e44ea68a573c4eb7b9d
Ashithdeveloper <ashithashith593@gmail.com>
2025-10-12 13:27:36 +0530

    go

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,7 +1,7 @@
 // import cron from "cron";
 // import https from "https";
 // import dotenv from "dotenv";
-
+//ijhjj
 // dotenv.config();
 
 // const job = new cron.CronJob("*/14 * * * *", function () {
